e9d7e28890b8d8cbe595f9967d319371ec3b5b90
Kaleb <kalebjean22@gmail.com>
2022-11-27 16:55:39 -0300

    configurado banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'easy-db.ctoesfdrxzlm.sa-east-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'admin',
+      password: 'root',
+      database: 'doesangue',
+      schema: 'public',
+      autoLoadModels: true,
+      synchronize: true,
+      timezone: 'America/Fortaleza',
+      logging: true,
+    }),
+    AuthModule,
+    UsersModule,
+    RolesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
